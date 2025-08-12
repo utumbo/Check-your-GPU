b429807455ca887dddbc64c412561e13ac29959e
utumbo <121032782+utumbo@users.noreply.github.com>
2025-08-12 22:27:44 +0700

    update main.py

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 import GPUtil
 
-gpus = GPUtil.getGPUs()
 def gpu_info():
+
+    gpus = GPUtil.getGPUs()
+    result = []
+
     for gpu in gpus:
-        print(f'ID: {gpu.id}, Name: {gpu.name}')
-        print(f'GPU Load: {gpu.load}') # загрузка gpu в процентах
-        print(f'GPU memoty Total: {gpu.memoryTotal}, GPU Temp: {gpu.temperature}')
-        print(f'GPU memory: {gpu.memoryUsed}, GPU free: {gpu.memoryFree}')
+        gpu_data = (
+            f'ID: {gpu.id}, Name: {gpu.name}'
+            f'GPU Load: {gpu.load}' # загрузка gpu в процентах
+            f'GPU memoty Total: {gpu.memoryTotal}, GPU Temp: {gpu.temperature}'
+            f'GPU memory: {gpu.memoryUsed}, GPU free: {gpu.memoryFree}'
+        )
+        result.append(gpu_data)
+    return result
+        
 
 if __name__ == "__main__":
-    print(gpu_info())
+    # Получаем данные
+    gpu_data = gpu_info()
+    
+    # Выводим информацию
+    print(gpu_data)
 
 
 # Загрузка GPU — проценте использования вычислительных ресурсов GPU
